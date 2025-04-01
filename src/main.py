0eb48c9e464a4c165ac8d47dfaefdd642e2013de
okodoon <31516823+okodoon@users.noreply.github.com>
2025-04-02 01:17:33 +0900

    __init__.pyの追加

--- src/main.py
+++ src/main.py
@@ -1,28 +1,27 @@
 import src.config.config as config
-from utils.text_extractor import extract_text_from_pdf
+from src.utils.text_extractor import extract_text_from_pdf
 import duckdb
 import csv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from src.minutes_divide_processor.models import (
-    SectionInfo, SectionInfoList, SectionString, SectionStringList, RedivideSectionString, RedivideSectionStringList,
-    RedividedSectionInfo, RedividedSectionInfoList, SpeakerAndSpeechContent, SpeakerAndSpeechContentList, MinutesProcessState
-)
 from src.minutes_divide_processor.minutes_process_agent import MinutesProcessAgent
+import os
 
 # config.pyを呼び出して環境変数を設定
 config.set_env()
 
 # PDF ファイルを読み込み
 try:
-    with open('minutes.pdf', 'rb') as f:
+    with open('data/minutes.pdf', 'rb') as f:
         file_content = f.read()
 except FileNotFoundError:
-    print(f"File not found: {pdf_path}")
+    print("Source file not found: data/minutes.pdf")
     exit()
 extracted_text = extract_text_from_pdf(file_content)
 
 def convert_to_csv(speaker_and_speech_content_list, output_file):
-    with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+    output_path = f'data/output/{output_file}'
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Speaker', 'Speech Content', 'Speech Order'])
         for item in speaker_and_speech_content_list:
@@ -35,8 +34,7 @@ def main():
   # CSVファイルに変換
   output_file = 'output.csv'
   convert_to_csv(speaker_and_speech_content_list, output_file)
-  print(f"CSVファイルに変換しました: {output_file}")
-  pop = duckdb.read_csv("output.csv")
+  print(f"CSVファイルに変換しました: data/output/{output_file}")
   return speaker_and_speech_content_list
 
 speaker_and_speech_content_list = main()
